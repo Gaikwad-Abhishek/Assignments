84be2167a446bdebaf62f0c760dc045afd03e0ca
abhishek.sg <abhishek.sg@prodapt.com>
2023-09-06 09:00:49 +0530

    EWD-Day7TakeHome

---
+++ EnterpriseWebDevelopment/Day7/TakeHome/learningspring/src/main/java/com/prodapt/learningspring/controller/ForumController.java
@@ -0,0 +1,140 @@
+package com.prodapt.learningspring.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.prodapt.learningspring.business.LoggedInUser;
+import com.prodapt.learningspring.controller.binding.AddPostForm;
+import com.prodapt.learningspring.controller.exception.ResourceNotFoundException;
+//import com.prodapt.learningspring.entity.CommentId;
+//import com.prodapt.learningspring.entity.CommentRecord;
+import com.prodapt.learningspring.entity.LikeId;
+import com.prodapt.learningspring.entity.LikeRecord;
+import com.prodapt.learningspring.entity.Post;
+import com.prodapt.learningspring.entity.User;
+//import com.prodapt.learningspring.repository.CommentCRUDRepository;
+import com.prodapt.learningspring.repository.LikeCRUDRepository;
+import com.prodapt.learningspring.repository.LikeCountRepository;
+import com.prodapt.learningspring.repository.PostRepository;
+import com.prodapt.learningspring.repository.UserRepository;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.ServletException;
+
+@Controller
+@RequestMapping("/forum")
+public class ForumController {
+  
+  @Autowired
+  private UserRepository userRepository;
+  
+  @Autowired
+  private PostRepository postRepository;
+  
+  @Autowired
+  private LikeCountRepository likeCountRepository;
+  
+  @Autowired
+  private LikeCRUDRepository likeCRUDRepository;
+  
+  @Autowired
+  private LoggedInUser loggedInUser;
+  
+//  @Autowired
+//  private CommentCRUDRepository commentCRUDRepository ;
+  
+  private List<User> userList;
+  
+//  @PostConstruct
+//  public void init() {
+//    userList = new ArrayList<>();
+//  }
+  
+  @GetMapping("/post/form")
+  public String getPostForm(Model model) {
+	  if (this.loggedInUser.getLoggedInUser() == null) {
+          return "redirect:/loginpage";
+      }
+	userList = new ArrayList<>();
+    model.addAttribute("postForm", new AddPostForm());
+    userRepository.findAll().forEach(user -> userList.add(user));
+    model.addAttribute("userList", userList);
+    model.addAttribute("authorid", 0);
+    return "forum/postForm";
+  }
+  
+  @PostMapping("/post/add")
+  public String addNewPost(@ModelAttribute("postForm") AddPostForm postForm, BindingResult bindingResult, RedirectAttributes attr) throws ServletException {
+    if (bindingResult.hasErrors()) {
+      System.out.println(bindingResult.getFieldErrors());
+      attr.addFlashAttribute("org.springframework.validation.BindingResult.post", bindingResult);
+      attr.addFlashAttribute("post", postForm);
+      return "redirect:/forum/post/form";
+    }
+    Optional<User> user = userRepository.findById(postForm.getUserId());
+    if (user.isEmpty()) {
+      throw new ServletException("Something went seriously wrong and we couldn't find the user in the DB");
+    }
+    Post post = new Post();
+    post.setAuthor(user.get());
+    post.setContent(postForm.getContent());
+    postRepository.save(post);
+    
+    return String.format("redirect:/forum/post/%d", post.getId());
+  }
+  
+  @GetMapping("/post/{id}")
+  public String postDetail(@PathVariable int id, Model model) throws ResourceNotFoundException {
+    Optional<Post> post = postRepository.findById(id);
+    if (post.isEmpty()) {
+      throw new ResourceNotFoundException("No post with the requested ID");
+    }
+    model.addAttribute("post", post.get());
+    model.addAttribute("userList", userList);
+    //int numLikes = likeCountRepository.countByPostId(id);
+    int numLikes = likeCRUDRepository.countByLikeIdPost(post.get());
+    model.addAttribute("likeCount", numLikes);
+    return "forum/postDetail";
+  }
+  
+  @PostMapping("/post/{id}/like")
+  public String postLike(@PathVariable int id, Long likerId, RedirectAttributes attr) {
+	if (this.loggedInUser.getLoggedInUser() == null) {
+          return "redirect:/loginpage";
+    }
+	LikeId likeId = new LikeId();
+    User user = this.loggedInUser.getLoggedInUser();
+    likeId.setUser(userRepository.findById(user.getUserId()).get());
+    likeId.setPost(postRepository.findById(id).get());
+    LikeRecord like = new LikeRecord();
+    like.setLikeId(likeId);
+    likeCRUDRepository.save(like);
+    return String.format("redirect:/forum/post/%d", id);
+  }
+  
+//  @PostMapping("/post/{id}/comment")
+//  public String addNewComment(@PathVariable int id, Integer CommenterId,@RequestParam("comment") String comment, RedirectAttributes attr) {
+//	CommentId commentId = new CommentId();
+//	commentId.setUser(userRepository.findById(CommenterId).get());
+//	commentId.setPost(postRepository.findById(id).get());
+//	CommentRecord commentRecord = new CommentRecord();
+//	commentRecord.setCommentId(commentId);
+//	commentRecord.setComment(comment);
+//	commentCRUDRepository.save(commentRecord);
+//    return "forum/commentForm";
+//  }
+}
---
+++ EnterpriseWebDevelopment/Day7/TakeHome/learningspring/src/main/java/com/prodapt/learningspring/repository/CommentCRUDRepository.java
@@ -0,0 +1,13 @@
+//package com.prodapt.learningspring.repository;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.prodapt.learningspring.entity.CommentId;
+//import com.prodapt.learningspring.entity.CommentRecord;
+//import com.prodapt.learningspring.entity.LikeId;
+//import com.prodapt.learningspring.entity.LikeRecord;
+//import com.prodapt.learningspring.entity.Post;
+//
+//public interface CommentCRUDRepository extends CrudRepository<CommentRecord, CommentId>{
+//	  
+//}
